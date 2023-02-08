e189bba284353ab1d050cc53ff84686d5b2fbec8
Infina <49457443+InfinaMii@users.noreply.github.com>
2023-02-08 21:39:48 +0000

    Add Trainer Vanity
    - added Trainer Cap, Torso + Legs
    - (hair drawn underneath hat)
    - Fixed the mod icon which was at 1/2 scale and so wouldn't load ingame

---
+++ Items/Vanity/TrainerCap.cs
@@ -0,0 +1,28 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerramonMod.Items.Vanity
+{
+	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
+	[AutoloadEquip(EquipType.Head)]
+	public class TrainerCap : ModItem
+	{
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Pokemon Trainer Cap");
+			ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true;
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 18;
+			Item.height = 18;
+			Item.value = 3000;
+			Item.rare = ItemRarityID.White;
+			Item.vanity = true;
+		}
+	}
+}
---
+++ Items/Vanity/TrainerTorso.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TerramonMod.Items.Vanity
+{
+	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
+	[AutoloadEquip(EquipType.Body)]
+	public class TrainerTorso : ModItem
+	{
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Pokemon Trainer Jacket");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 18;
+			Item.height = 18;
+			Item.value = 3000;
+			Item.rare = ItemRarityID.White;
+			Item.vanity = true;
+		}
+	}
+}
